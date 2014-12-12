f40f0522b7973db1162846cf96bf7bb43fb94b7e
Pride Leong <lykling.lyk@gmail.com>
2014-12-12 14:44:25 +0800

    find

--- spec/memsetSpec.js
+++ spec/memsetSpec.js
@@ -6,33 +6,129 @@ var db = require('../src');
 
 var data = [];
 
-for (var i = 0; i < 15; i++) {
+for (var i = 0; i < 100000; i++) {
     data.push({
+        _id: i,
         name: 'user' + i,
         age: i + 20,
-        mod: i % 5
+        mod: i % 100
     });
 }
 
 db.create('user', data);
 
 describe('jasmine-node', function () {
-    var item = db.user.find({age: 21});
-    var items = db.user.find([{mod: 3}]);
-    var all = db.user.find({name: true});
-    var parts = db.user.find(3, 5);
+    it('Testing find all', function () {
+        var items;
+        items = db.user.find();
+        expect(items.length).toBe(data.length)
 
-    it('test find memset', function () {
-        expect(item.length).toBe(1);
-        expect(items.length).toBeGreaterThan(2);
-        expect(all.length).toBe(15);
-        expect(parts.length).toBe(5);
+        items = db.user.find({});
+        expect(items.length).toBe(data.length)
     });
 
-    db.user.remove({mod: 3});
-    
-    it('test remove memset', function () {
-        expect(db.user.length).toBeLessThan(13);
+    it('Testing find invalid field', function () {
+        var items;
+
+        items = db.user.find({ invalueField: true });
+        expect(items.length).toBe(0)
+
+        items = db.user.find({ invalueField: true, age: 55 });
+        expect(items.length).toBe(0)
     });
 
+    it('Testing find one field', function () {
+        var items;
+
+        items = db.user.find({ _id: { $eq: 90 } });
+        expect(items.length).toBe(1);
+
+        items = db.user.find({ age: { $eq: 30 } });
+        expect(items.length).toBe(1);
+
+        items = db.user.find({ mod: { $eq: 3 } });
+        expect(items.length).toBe(1000);
+
+        items = db.user.find({ name: { $eq: 'user99' } });
+        expect(items.length).toBe(1);
+    });
+
+    it('Testing find with regexp', function () {
+        var items;
+
+        items = db.user.find({ name: /user3[1-5]$/ });
+        expect(items.length).toBe(5);
+
+        items = db.user.find({ name: /user[0-9]+/ });
+        expect(items.length).toBe(data.length);
+
+        items = db.user.find({ age: /^99$/ });
+        expect(items.length).toBe(1);
+
+        items = db.user.find({ age: /abc/ });
+        expect(items.length).toBe(0);
+    });
+
+    it('Testing find with number', function () {
+        var items;
+
+        items = db.user.find({ age: 39 });
+        expect(items.length).toBe(1);
+        expect(items[0].name).toBe('user19');
+
+        items = db.user.find({ mod: 99 });
+        expect(items.length).toBe(1000);
+
+        items = db.user.find({ name: 89 });
+        expect(items.length).toBe(0);
+    });
+
+    it('Testing find with string', function () {
+        var items;
+
+        items = db.user.find({ name: 'user39' });
+        expect(items.length).toBe(1);
+        expect(items[0].age).toBe(59);
+
+        items = db.user.find({ age: '39' });
+        expect(items.length).toBe(0);
+
+        items = db.user.find({ mod: '99' });
+        expect(items.length).toBe(0);
+    });
+
+    it('Testing find with boolean', function () {
+        var items;
+
+        items = db.user.find({ age: true });
+        expect(items.length).toBe(data.length);
+
+        items = db.user.find({ age: false });
+        expect(items.length).toBe(0);
+
+        items = db.user.find({ invalidField: true });
+        expect(items.length).toBe(0);
+
+        items = db.user.find({ invalidField: false });
+        expect(items.length).toBe(data.length);
+    });
+
+    it('Testing find with criteria', function () {
+        var items;
+
+        items = db.user.find({ age: { $lt: 10 } });
+        expect(items.length).toBe(0);
+
+        items = db.user.find({ age: { $lte: 30 } });
+        expect(items.length).toBe(11);
+
+        items = db.user.find({ age: { $gt: 99 } });
+        expect(items.length).toBe(data.length - 80);
+
+        items = db.user.find({ age: { $gte: 99 } });
+        expect(items.length).toBe(data.length - 79);
+
+        items = db.user.find({ age: { $eq: 99 } });
+        expect(items.length).toBe(1);
+    });
 });
--- src/memset.js
+++ src/memset.js
@@ -1,51 +1,108 @@
 /**
  * memset: dataset in memory
- * 
+ *
  * @author: Liandong Liu (liu@liandong.org)
+ * @author: Pride Leong<lykling.lyk@gmail.com>
  */
 
 /**
  * Memset
  *
+ * @param {Object} data data set
  * @constructor
  */
 function Memset(data) {
     this.set = [].concat(data);
 }
 
-function matchExtact(data, selector) {
-    for (var key in selector) {
-        if (selector[key] === true && data[key]) {
-            continue;
-        } else if (data[key] != selector[key]) {
-            return false;
-        }
+var conditionMap = {
+    $gt: function (valueA, valueB) {
+        return valueA > valueB;
+    },
+    $gte: function (valueA, valueB) {
+        return valueA >= valueB;
+    },
+    $lt: function (valueA, valueB) {
+        return valueA < valueB;
+    },
+    $lte: function (valueA, valueB) {
+        return valueA <= valueB;
+    },
+    $eq: function (valueA, valueB) {
+        return valueA === valueB;
     }
-    return true;
-}
+};
 
-function matchSome(selectors) {
+/**
+ *
+ * @param {Object} criteria
+ *  Specifies selection criteria using query operators.
+ * @return {Function} callback
+ *  Function to test each element of the array.
+ *  Return true to keep the element, false otherwise
+ */
+function matcher(criteria) {
+    var fields = Object.keys(criteria || {});
     return function (item, index) {
-        item._id = index;
-        return selectors.some(function (selector) {
-            return matchExtact(item, selector);
+        return fields.every(function (field) {
+            var condition = criteria[field];
+            var exist = item.hasOwnProperty(field);
+            var type = Object.prototype.toString.call(condition);
+            switch (type) {
+                case '[object Boolean]':
+                    return condition === exist;
+                case '[object String]':
+                    return condition === item[field];
+                case '[object Number]':
+                    return condition === item[field];
+                case '[object RegExp]':
+                    return condition.test(item[field]);
+                case '[object Object]':
+                    var valueA = item[field];
+                    var conditionKeys = Object.keys(condition);
+                    return conditionKeys.every(function (key) {
+                        var cmpFunc = conditionMap[key];
+                        var valueB = condition[key];
+                        if ('function' === typeof cmpFunc) {
+                            return cmpFunc(valueA, valueB);
+                        }
+                        return true;
+                    });
+                default:
+                    return true;
+            }
         });
-    }
+    };
 }
 
 Memset.prototype = {
-    find: function (selector, count) {
-        if ('object' !== typeof selector) {
-            var idx = +selector;
-            count || (count = 1);
-            return this.set.slice(idx, idx + count);
+    /**
+     * @method find
+     * @param {Object} criteria
+     *  Specifies selection criteria using query operators.
+     * @param {Object} projection
+     *  Specifies the fields to return using projection operators
+     * @return {Array.<Object>} data set
+     */
+    find: function (criteria, projection) {
+        var set = this.set.filter(matcher(criteria));
+        var result = [];
+        if ('object' === typeof projection) {
+            var keys = Object.keys(projection);
+            set.forEach(function (item) {
+                var value = {};
+                keys.forEach(function (key) {
+                    value[key] = item[key];
+                });
+                result.push(value);
+            });
+            set = result;
         }
-        var selectors = [].concat(selector);
-        return this.set.filter(matchSome(selectors));
+        return set;
     },
-    remove: function (selector) {
+    remove: function (criteria) {
         var set = this.set;
-        var lst = this.find(selector);
+        var lst = this.find(criteria);
         lst.forEach(function (item) {
             set[item._id] = null;
         });
